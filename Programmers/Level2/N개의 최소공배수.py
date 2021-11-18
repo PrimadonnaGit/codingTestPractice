89a665b3f6899298747e8716199c416c194606c1
primadonna <jwhyun2215@gmail.com>
2021-11-18 18:38:19 +0900

    lcd, gcd

---
+++ Programmers/Level2/N개의 최소공배수.py
@@ -0,0 +1,19 @@
+import math
+
+
+def solution(arr):
+    answer = 0
+    answer = arr[0]
+    # lcd = (x*y) / gcd(x,y)
+    for num in arr:
+        answer = answer * num // math.gcd(answer, num)
+    return answer
+
+
+if __name__ == '__main__':
+    arr = [2, 6, 8, 14]
+
+    print(solution(arr))
+
+    arr = [1, 2, 3]
+    print(solution(arr))
