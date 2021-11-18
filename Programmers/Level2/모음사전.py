5a1d9ceb91cfa4089faef69721180ccb02bea77d
primadonna <jwhyun2215@gmail.com>
2021-11-18 23:04:17 +0900

    내장 함수 (itertools.product)

---
+++ Programmers/Level2/모음사전.py
@@ -0,0 +1,22 @@
+from itertools import product
+
+
+def solution(word):
+    pivot = 'AEIOU'
+    dictionary = []
+    for i in range(1, len(pivot) + 1):
+        dictionary += [''.join(w) for w in product(pivot, repeat=i)]
+
+    dictionary.sort()
+    return dictionary.index(word) + 1
+
+
+if __name__ == '__main__':
+    word = "AAAAE"
+    print(solution(word))
+    word = "AAAE"
+    print(solution(word))
+    word = "I"
+    print(solution(word))
+    word = "EIO"
+    print(solution(word))
