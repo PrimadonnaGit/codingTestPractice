b9b235fb62a4c70238020c7441d4ccc3ce8c6e52
primadonna <jwhyun2215@gmail.com>
2021-11-18 23:40:06 +0900

    괄호 처리

---
+++ Programmers/Level2/올바른 괄호.py
@@ -0,0 +1,26 @@
+def solution(s):
+    answer = True
+    left_count = 0
+    right_count = 0
+    for c in s:
+        if c == '(':
+            left_count += 1
+        else:
+            right_count += 1
+
+        if right_count > left_count:
+            return False
+    if left_count != right_count:
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    s = "()()"
+    assert solution(s) == True
+    s = "(())()"
+    assert solution(s) == True
+    s = ")()("
+    assert solution(s) == False
+    s = "(()("
+    assert solution(s) == False
